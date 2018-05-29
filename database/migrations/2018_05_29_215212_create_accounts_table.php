39c5498edb9f0e13597bbe6a18204b2de7852112
Gabriel Alves <gabriel.alves@grupomed.com.br>
2018-05-29 18:57:30 -0300

    to indo pq minha namo ta apressando

---
+++ database/migrations/2018_05_29_215212_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->increments('title', 45);
+            $table->increments('agency');
+            $table->increments('account_number');
+            $table->increments('balance',11,2);
+            $table->increments('balance_initial', 11, 2);
+            $table->increments('bank_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
