7ce1ca30be15d144937238b60fb0257fa906940c
Gabriel Alves <gabriel.alves@grupomed.com.br>
2018-05-30 11:59:26 -0300

    Encerrado!!

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
+use Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Passport::tokensExpireIn(Carbon::now()->addHours(1));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(1));
         //
     }
 }
